67d204e68662d84eefc5835443e552451419bac6
Lauri K <laurikamm@gmail.com>
2020-11-19 16:03:40 +0200

    lesson01

---
+++ Lesson01/script.js
@@ -0,0 +1,10 @@
+let money = 1337;
+const income = 'huge';
+let addExpenses = ['low'];
+let deposit = 187;
+let mission = 'completed';
+let period = 'half';
+
+alert('Hello World!');
+console.log('Lesson 1');
+
