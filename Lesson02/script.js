3b8fc7b7ea556fb5c66231103451bf101256b793
Lauri K <laurikamm@gmail.com>
2020-11-20 13:31:21 +0200

    lesson02

---
+++ Lesson02/script.js
@@ -0,0 +1,17 @@
+let money = 1337;
+let income = 'freelance';
+let addExpenses = 'internet,food';
+let deposit = true;
+let mission = 20000;
+let period = 12;
+
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+console.log(addExpenses.length);
+console.log(`Период равен ${period} месяцев и Цель заработать ${mission} евро`);
+
+console.log(addExpenses.toLowerCase().split(''));
+
+let budgetDay = (3000 / 30);
+console.log(budgetDay);
