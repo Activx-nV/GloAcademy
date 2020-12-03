95335be45b5b09a491e3d663d909113daa5b931b
Activx-nV <68726573+Activx-nV@users.noreply.github.com>
2020-12-03 19:54:08 +0200

    feat: make basic functional

--- Lesson10/script/script.js
+++ Lesson10/script/script.js
@@ -2,7 +2,10 @@
 
 const list = document.querySelectorAll('ul'),
     books = document.querySelectorAll('.book'),
-    advertisment = document.querySelector('.adv');
+    advertisment = document.querySelector('.adv'),
+    list2 = books[0].children[1].children,
+    list5 = books[5].children[1].children;
+
 
 advertisment.remove();
 
@@ -10,58 +13,20 @@ document.body.style.backgroundImage = "url('./image/you-dont-know-js.jpg')";
 
 books[4].firstElementChild.innerHTML = "<a href=\"https://github.com/azat-io/you-dont-know-js-ru/blob/master/this%20%26%20object%20prototypes/README.md#you-dont-know-js-this--object-prototypes\" target=\"_blank\">Книга 3. this и Прототипы Объектов</a>";
 
-const secondBook1 = list[0].children[0].cloneNode(true);
-const secondBook2 = list[0].children[1].cloneNode(true);
-const secondBook3 = list[0].children[2].cloneNode(true);
-const secondBook4 = list[0].children[3].cloneNode(true);
-const secondBook5 = list[0].children[4].cloneNode(true);
-const secondBook6 = list[0].children[5].cloneNode(true);
-const secondBook7 = list[0].children[6].cloneNode(true);
-const secondBook8 = list[0].children[7].cloneNode(true);
-const secondBook9 = list[0].children[8].cloneNode(true);
-const secondBook10 = list[0].children[9].cloneNode(true);
-const secondBook11 = list[0].children[10].cloneNode(true);
-
-const fifthBook1 = list[5].children[0].cloneNode(true);
-const fifthBook2 = list[5].children[1].cloneNode(true);
-const fifthBook3 = list[5].children[2].cloneNode(true);
-const fifthBook4 = list[5].children[3].cloneNode(true);
-const fifthBook5 = list[5].children[4].cloneNode(true);
-const fifthBook6 = list[5].children[5].cloneNode(true);
-const fifthBook7 = list[5].children[6].cloneNode(true);
-const fifthBook8 = list[5].children[7].cloneNode(true);
-const fifthBook9 = list[5].children[8].cloneNode(true);
-const fifthBook10 = list[5].children[9].cloneNode(true);
-const fifthBook11 = list[5].children[10].cloneNode(true);
-
-
-list[0].children[2].outerHTML = secondBook4.outerHTML;
-list[0].children[3].outerHTML = secondBook7.outerHTML;
-list[0].children[4].outerHTML = secondBook9.outerHTML;
-list[0].children[5].outerHTML = secondBook5.outerHTML;
-list[0].children[6].outerHTML = secondBook6.outerHTML;
-list[0].children[7].outerHTML = secondBook8.outerHTML;
-list[0].children[8].outerHTML = secondBook10.outerHTML;
-list[0].children[9].outerHTML = secondBook3.outerHTML;
-
-list[5].children[2].outerHTML = fifthBook10.outerHTML;
-list[5].children[5].outerHTML = fifthBook3.outerHTML;
-list[5].children[8].outerHTML = fifthBook6.outerHTML;
-list[5].children[9].outerHTML = fifthBook9.outerHTML;
+list2[1].after(list2[3]);
+list2[2].after(list2[6]);
+list2[3].after(list2[8]);
+list2[4].after(list2[6]);
+list2[9].after(list2[6]);
 
 list[2].children[list[2].children.length - 2].insertAdjacentHTML('beforeend', '<li>Глава 8: За пределами ES6</li>');
 
-
-const book1 = books[1].cloneNode(true);
-const book2 = books[0].cloneNode(true);
-const book3 = books[4].cloneNode(true);
-const book4 = books[3].cloneNode(true);
-const book5 = books[5].cloneNode(true);
-const book6 = books[2].cloneNode(true);
+list5[1].after(list5[9]);
+list5[2].after(list5[4]);
+list5[3].after(list5[5]);
+list5[8].after(list5[6]);
 
 
-books[1].replaceWith(book2);
-books[0].replaceWith(book1);
-books[2].replaceWith(book3);
-books[4].replaceWith(book5);
-books[5].replaceWith(book6);
+books[1].after(books[0]);
+books[0].after(books[4]);
+books[5].after(books[2]);
