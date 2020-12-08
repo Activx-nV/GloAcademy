b29011e2654e346a64d86f58c44bf0cc3e486818
Activx-nV <68726573+Activx-nV@users.noreply.github.com>
2020-12-08 20:45:45 +0200

    feat: make basic functional

--- Lesson14_extra/script.js
+++ Lesson14_extra/script.js
@@ -23,7 +23,7 @@ DomElement.prototype.createElement = function () {
         div.style.fontSize = this.fontSize + 'px';
         div.textContent = this.text;
 
-        document.body.append(div);
+        document.body.prepend(div);
     } else if (this.selector[0] === '#') {
         let p = document.createElement('p');
 
@@ -34,94 +34,41 @@ DomElement.prototype.createElement = function () {
         p.style.fontSize = this.fontSize + 'px';
         p.textContent = this.text;
 
-        document.body.append(p);
+        document.body.prepend(p);
     }
 };
 
-DomElement.prototype.getContext = function (callback) {
-    let thisObj = this;
-    return (function () {
-        callback.apply(thisObj);
-    });
-};
-
-
-
-
-//use id selector - #divElement or class selector - .divElement
-
-const DOMElement = new DomElement('#divElement', 100, 100, 'cyan', 20, 'Hello World');
-
-
-let applied1 = DOMElement.getContext(DOMElement.createElement);
-
-document.addEventListener('DOMContentLoaded', applied1);
-
-
-
-document.addEventListener('keydown', (e) => {
-    let p = document.getElementById('divElement');
-    let div = document.querySelector('div');
-
-    if (div === null) {
+DomElement.prototype.moveBlock = function (element) {
+    document.addEventListener('keydown', (e) => {
         if (e.keyCode === 38) {
-            // up arrow
-            p.style.top = counterVertical + 'px';
+            document.body.firstChild.style.top = counterVertical + 'px';
             if (counterVertical < 0) {
                 counterVertical = 0;
             }
             counterVertical -= 10;
         } else if (e.keyCode === 40) {
-            // down arrow
-            p.style.top = counterVertical + 'px';
+            document.body.firstChild.style.top = counterVertical + 'px';
             if (counterVertical > 850) {
                 counterVertical = 850;
             }
             counterVertical += 10;
         } else if (e.keyCode === 37) {
-            // left arrow
-            p.style.left = counterHorizontal + 'px';
+            document.body.firstChild.style.left = counterHorizontal + 'px';
             if (counterHorizontal < 10) {
                 counterHorizontal = 10;
             }
             counterHorizontal -= 10;
         } else if (e.keyCode === 39) {
-            // right arrow
-            p.style.left = counterHorizontal + 'px';
+            document.body.firstChild.style.left = counterHorizontal + 'px';
             if (counterHorizontal > 1820) {
                 counterHorizontal = 1820;
             }
             counterHorizontal += 10;
         }
-    } else if (p === null) {
-        if (e.keyCode === 38) {
-            // up arrow
-            div.style.top = counterVertical + 'px';
-            if (counterVertical < 10) {
-                counterVertical = 10;
-            }
-            counterVertical -= 10;
-        } else if (e.keyCode === 40) {
-            // down arrow
-            div.style.top = counterVertical + 'px';
-            if (counterVertical > 850) {
-                counterVertical = 850;
-            }
-            counterVertical += 10;
-        } else if (e.keyCode === 37) {
-            // left arrow
-            div.style.left = counterHorizontal + 'px';
-            if (counterHorizontal < 10) {
-                counterHorizontal = 10;
-            }
-            counterHorizontal -= 10;
-        } else if (e.keyCode === 39) {
-            // right arrow
-            div.style.left = counterHorizontal + 'px';
-            if (counterHorizontal > 1820) {
-                counterHorizontal = 1820;
-            }
-            counterHorizontal += 10;
-        }
-    }
-});
+    });
+};
+
+const DOMElement = new DomElement('#divElement', 100, 100, 'cyan', 20, 'Hello World');
+
+document.addEventListener('DOMContentLoaded', DOMElement.createElement.apply(DOMElement));
+DOMElement.moveBlock();
